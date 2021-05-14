28b8b92c7ff67f3dbf582dd7588cfc4038f66e57
Devaprasad Adapa <dada@stibosystems.com>
2021-05-14 20:04:31 +0530

    master sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into course (id, name) values(101, 'Basics of Computer Concepts');
 insert into course (id, name) values(102, 'OOPS Made Easy');
 insert into course (id, name) values(103, 'Agorithms & DS');
-insert into course (id, name) values(104, 'Javascript for dummies');
+insert into course (id, name) values(104, 'Javascript for Dummies');
+insert into course (id, name) values(105, 'Let Us C++');
