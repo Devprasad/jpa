9124ca8cf8a9be93ecc33500b7d9d27cfa51ea55
Devaprasad Adapa <dada@stibosystems.com>
2021-05-14 20:18:00 +0530

    master space

--- src/main/java/com/dada/database/dbone/student/StudentApplication.java
+++ src/main/java/com/dada/database/dbone/student/StudentApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class StudentApplication implements CommandLineRunner {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
